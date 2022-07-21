b0e74f78c3a779b74f2866853d4ca31632ed69fb
konuralpd <98783085+konuralpd@users.noreply.github.com>
2022-07-21 23:03:42 +0300

    Initial Commit

---
+++ StarbucksWonder/StarbucksWonderApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarbucksWonderApp.swift
+//  StarbucksWonder
+//
+//  Created by Mac on 21.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct StarbucksWonderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
